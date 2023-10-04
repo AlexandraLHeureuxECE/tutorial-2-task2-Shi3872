1a10c947ecf5ffa8125564ee8fdb91d04d3a1876
Jennifer <jshi372@uwo.ca>
2023-10-03 21:08:44 -0400

    Finished assignment

--- callbackFunctions.js
+++ callbackFunctions.js
@@ -6,13 +6,20 @@ let triangleFrame = function(letter)
     {
         let line='';
         // write a loop to start the line with 3 - i spaces
-       
+       for(let j = 3-i; j >0; j--){
+            framedLetter+= " ";
+       }
 
         // The third line will contain the framed letter so we need to set the right condition
-        if (/*set the condition to specify the third line */) 
+        if (i == 2) {//set the condition to specify the third line 
             line += `* ${letter} *`;
+        }
+            
         else
         {
+            for(let j = 2*i + 1; j > 0; j--){
+               framedLetter += "*";
+            }
             // the rest of the lines of the triangle would contain all stars
             // create a loop to add 2i + 1 stars to the line
         }
@@ -29,7 +36,9 @@ let rectangleFrame = function(letter)
     line = "";
 
     //write a loop the add five stars to the line
-   
+   for(let i = 0; i < 5; i++){
+        line += "*";
+   }
 
     //set the framed letter to be of 3 lines (one line of star, 
     //one line * Letter * and the last line of all stars)
@@ -43,12 +52,19 @@ let rectangleFrame = function(letter)
 //      - the first argument is a callback function that creates the desired frame
 //      - the second argument is the letter to be framed.
 // The retun should be the string of the letter framed by the given framer.
-let generalFramer;
+let generalFramer = function(x,y){
+    if(x == 'triangle'){
+        return triangleFrame(y);
+    }
+    else if(x == 'rectangle'){
+        return rectangleFrame(y);
+    }
+}
 
-console.log(generalFramer(/*set the argument to frame the letter A in a triangle*/));
-console.log(generalFramer(/*set the argument to frame the letter B in a triangle*/));
-console.log(generalFramer(/*set the argument to frame the letter C in a triangle*/));
+console.log(generalFramer('triangle','A'/*set the argument to frame the letter A in a triangle*/));
+console.log(generalFramer('triangle','B'/*set the argument to frame the letter B in a triangle*/));
+console.log(generalFramer('triangle','C'/*set the argument to frame the letter C in a triangle*/));
 
-console.log(generalFramer(/*set the argument to frame the letter A in a rectangle*/));
-console.log(generalFramer(/*set the argument to frame the letter B in a rectangle*/));
-console.log(generalFramer(/*set the argument to frame the letter C in a rectangle*/));
+console.log(generalFramer('rectangle','A'/*set the argument to frame the letter A in a rectangle*/));
+console.log(generalFramer('rectangle','B'/*set the argument to frame the letter B in a rectangle*/));
+console.log(generalFramer('rectangle','C'/*set the argument to frame the letter C in a rectangle*/));
--- numberInspector.js
+++ numberInspector.js
@@ -14,7 +14,14 @@
     {
         // Complete the code so that based on a given string "palindrome, max, duplicate, divisibleBy" 
         // The appropriate function is returned. 
-     
+        case 'palindrome':
+            return palindrome;
+        case 'max':
+            return maxDigit;
+        case 'duplicate':
+            return duplicateNumber;
+        case 'divisibleBy':
+            return makeDivisibleBy; 
     }
 
      /*Write a function that returns the palindrome number of the number you are given, 
@@ -23,7 +30,16 @@
     *Keep in mind that you can access each digit of a number through simple operations. 
     *You can assume that this function will receive a number as a parameter. 
     *HINT: You can use Math.floor to get the integer closest to a numberr*/
-    function 
+    function palindrome(x){
+        let digit = 0;
+        let palindrome = 0;
+        while(x>0){
+            digit = x%10;
+            palindrome = (palindrome*10) + digit;
+            x = Math.floor(x/10);
+        }
+        return String(palindrome);
+    }
 
     /* Write a function that will return the maximum digit of the number you are given.
     * For example, if you are given 192345, it should return 9. If you are given 12645 it should return 6. 
@@ -31,7 +47,18 @@
     *You can assume that this function will receive a number as a parameter. 
     *HINT: You can use Math.floor to get the integer closest to a numberr*/
 
-    function 
+    function maxDigit(x){
+        let digit = 0;
+        let max = 0;
+        while(x>0){
+            digit = x%10;
+            if(digit > max){
+                max = digit;
+            }
+            x = Math.floor(x/10);
+        }
+        return Number(max);
+    }
     /*This function can be use to identify whether a number contains 2 or more consecutive duplicate numbers
     * For example, if you are given 1223 it should return yes because we have 22 in our number. Similarly, 
     * 12344 should also return true while 23452 should return false
@@ -39,7 +66,18 @@
     *You can assume that this function will receive a number as a parameter. 
     *HINT: You can use Math.floor to get the integer closest to a number, the remainder operator is also helful*/
      
-    function 
+    function duplicateNumber(x){
+        let xString = String(x);
+        let digit = xString[0];
+        for(let i = 0; i < xString.length; i++){
+            if(xString[i+1] == digit){
+                return true;
+            }else{
+                digit = xString[i+1];
+            }
+        }
+        return false;
+    }
     /*This function will return the value needed to be added to a number to make it divisible by the given dividor. 
     *For example, if you are given 63 and 5, we are looking for the number needed to make 63 divisible by 5. In this case, 
     the function should return 2. To challenged yourself, try to use the && or || operator.
@@ -47,7 +85,14 @@
     *You can assume that this function will receive a number as a parameter. 
     *HINT: You can use Math.floor to get the integer closest to a number, the remainder operator is also helful*/
 
-    function 
+    function makeDivisibleBy(x, y){
+        let amount = 0;
+        while(x%y != 0){
+            amount++;
+            x++;
+        }
+        return Number(amount);
+    }
 
 }
 
@@ -57,11 +102,10 @@
 // let MakeDivisibleBy = getNumberInvestigatingTool(/* call the NumberInvestigating tool so that it would return the function that will makeANumberDivisibleBy function*/); 
 
 
-let Palindrome = getNumberInvestigatingTool(/* call the NumberInvestigating tool so that it would return the palindrome making function */);
-let MaxDigit = getNumberInvestigatingTool(/* call the NumberInvestigating tool so that it would return the max digit finder function */);
-let DuplicateNumber = getNumberInvestigatingTool(/* call the NumberInvestigating tool so that it would return the double digit Check function*/);
-let MakeDivisibleBy = getNumberInvestigatingTool(/* call the NumberInvestigating tool so that it would return the function that will makeANumberDivisibleBy function*/); 
-
+let Palindrome = getNumberInvestigatingTool('palindrome');
+let MaxDigit = getNumberInvestigatingTool('max');
+let DuplicateNumber = getNumberInvestigatingTool('duplicate');
+let MakeDivisibleBy = getNumberInvestigatingTool('divisibleBy');    
 
 // don't change the three lines below
 
